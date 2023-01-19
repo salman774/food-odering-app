29068958d710be068f866ee2b024f9162a13913b
salman naveed <realmadridinmydna3@gmail.com>
2023-01-19 15:04:36 +0500

    code before finding object

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 import { menuArray } from "./fooddata.js";
 const menuSection = document.getElementById("menu-section");
 
-function cartbox() {}
+function taregetobjectofchai() {
+  menuSection.addEventListener("click", function (e) {
+    let taregetid = e.target.id;
+    console.log(taregetid);
+    return taregetid;
+  });
+}
+taregetobjectofchai();
+
+// menuArray.forEach(function (show) {
+//   let displayorder = "";
+
+//   displayorder += `<div class="order-detail">
+//     <p>${show.name}</p>
+//     <button type="reset">remove</button>
+//     <p>${show.price}</p>
+//   </div>
+//   <hr />
+//   <div class="total-price">
+//     <p>total-price</p>
+//     <p>value</p>
+//   </div>`;
+// });
 
 function renderMenu() {
   let fullmenu = "";
@@ -18,8 +40,8 @@ function renderMenu() {
         <hr />
         <p>${menu.price}</p>
         <hr />
-      </div>
-      <button class="add-to-order">+</button>
+      </div >
+      <button id="${menu.id}" class="add-to-order">+</button>
     </div>
   `;
   });
