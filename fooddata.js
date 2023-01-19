a60fe9e9772bb09bc482d905129883cae6cf3992
salman naveed <realmadridinmydna3@gmail.com>
2023-01-19 12:04:06 +0500

    changes chai information

--- fooddata.js
+++ fooddata.js
@@ -2,29 +2,28 @@ export const menuArray = [
   {
     name: "karak-chai",
     ingredients: [
-      " made with a blend of fermented black tea leaves ginger and spices black pepper cardamon, cinnamon, fennel and cloves",
+      " Made with a blend of fermented black tea leaves ginger and spices black pepper cardamon, cinnamon, fennel and cloves.The above info just went to uk for higher studies ",
     ],
     id: 0,
-    price: `300 rupess`,
+    price: 300,
     Image: "images/karakchai -PhotoRoom.png-PhotoRoom.png",
   },
   {
     name: "kashmiri-chai",
     ingredients: [
-      "Baking soda, which neutralizes the acid in the tea leaves and transforms the otherwise pale green tea into a deep burgundy color",
+      "Baking soda, which neutralizes the acid in the tea leaves and transforms the otherwise pale green tea into a deep burgundy color----Lol there is nothing like this in it",
     ],
     id: 1,
-    price: `500 rupess`,
+    price: 500,
     Image: "images/kashmiri-pink-chai-1-featured-PhotoRoom.png-PhotoRoom.png",
   },
   {
     name: "everyday-chai",
     ingredients: [
-      `Caffeine (traditional teas, not herbal)
-Polyphenols. Flavonols myricetin, quercetin, kaempferol. Theaflavins formed when black tea leaves are oxidized. Catechins found in green tea; epigallocatechin-3 gallate (EGCG) is the main form. `,
+      `Just making you stupid 'every-day' is such a scam we are just adding water in every-day and selling it to you for freaking 100 rs lol  `,
     ],
     id: 1,
-    price: `100 rupees`,
+    price: 150,
     Image: "images/everday.jpeg",
   },
 ];
