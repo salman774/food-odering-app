663e0c6c300304d881dabe854397566fb8fcde29
salman naveed <realmadridinmydna3@gmail.com>
2023-01-24 17:31:02 +0500

    added total amount

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { menuArray } from "./fooddata.js";
 const menuSection = document.getElementById("menu-section");
 const yourOrder = document.getElementById("your-order");
 let array = [];
+let totalPrice = document.getElementById("total-price");
 
 //* 1
 function renderMenu() {
@@ -17,7 +18,7 @@ function renderMenu() {
         <p>${menu.ingredients}
         </p>
         <hr />
-        <p>${menu.price}</p>
+        <p>${menu.price} Rupess</p>
         <hr />
       </div >
       <button class="add-to-order" data-add="${menu.id}">+</button>
@@ -61,9 +62,38 @@ function renderorderdetail() {
   <div class="order-detail">
     <p>${renderorder.name}</p>
    <button type="reset">remove</button>
-   <p>${renderorder.price}</p>
+   <p>${renderorder.price} RS. </p>
   </div>
   <hr/>`;
   });
   yourOrder.innerHTML = order;
+  totalpriceefunction();
+}
+
+// function totalpriceefunction() {
+//   let price = "";
+//   array.forEach(function (priceofchai) {
+//     price += ` <div id="total-price" class="total-price">
+//     <p>total-price:</p>
+//     <p>${priceofchai.price} </p>
+//   </div>`;
+//   });
+//   totalPrice.innerHTML = price;
+// }
+
+function totalpriceefunction() {
+  let price = "";
+  let result = array.map(function (price) {
+    return price.price;
+  });
+  let sum = 0;
+  for (let i = 0; i < result.length; i++) {
+    sum += result[i];
+  }
+  console.log(sum);
+  price += ` 
+        <p>Total-price:</p>
+        <p>${sum} rupees</p>
+      </div>`;
+  totalPrice.innerHTML = price;
 }
